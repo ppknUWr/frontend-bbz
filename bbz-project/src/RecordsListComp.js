0f1add6bb71581170228f864de64862a5dae2846
Wojtas399 <wojtekp26@gmail.com>
2021-03-04 21:16:22 +0100

    The main records list component with 3 panels. Top panel with inputs and buttons. Middle panel displaying records. Bottom panel with 2 buttons to change records page.

---
+++ bbz-project/src/RecordsListComp.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {useState} from "react";
+import './RecordsListStyles.css';
+import AddRecordButton from "./AddRecordButton";
+import ImportButton from "./ImportButton";
+import SelectInput from "./SearchBySelectTextField";
+import SearchInput from "./SearchTextField";
+
+const searchByValues = [
+  {
+    value: 'Autor',
+  },
+  {
+    value: 'Tytuł',
+  },
+  {
+    value: 'Rok',
+  },
+  {
+    value: 'Wydawca',
+  },
+]
+
+const RecordsListComp = () => {
+  const [searchByVal, setSearchByVal] = useState('Autor');
+
+  return (
+    <div className={'mainBody'}>
+      <div className={'topPanel'}>
+        <SearchInput/>
+        <SelectInput
+          value={searchByVal}
+          setVal={setSearchByVal}
+          allValues={searchByValues}
+        />
+        <ImportButton />
+        <AddRecordButton />
+      </div>
+      <div className={'middlePanel'}>
+        <div className={'recordsBck'}>
+          <div className={'sortOptions'}>
+
+          </div>
+          <div className={'recordsField'}>
+
+          </div>
+        </div>
+      </div>
+      <div className={'bottomPanel'}>
+        Panel dolny
+      </div>
+    </div>
+  )
+}
+
+export default RecordsListComp;
