cd0df64a9118ce39dcc6731294e4eb642e521d04
Wojtas399 <wojtekp26@gmail.com>
2021-03-05 00:03:38 +0100

    Come back to start project position.

--- bbz-project/src/App.js
+++ bbz-project/src/App.js
@@ -1,9 +1,24 @@
-import './Bck.css';
-import Bck from "./Bck";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Bck/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
