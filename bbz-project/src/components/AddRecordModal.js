2f703d89eb35c128560e59a6ee5a0e9bcc394d1c
xdexer <308167@uwr.edu.pl>
2021-05-03 19:23:05 +0200

    version 1 of record adding form with no functionality

--- bbz-project/src/components/AddRecordModal.js
+++ bbz-project/src/components/AddRecordModal.js
@@ -4,46 +4,33 @@ import TextField from "@material-ui/core/TextField";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/NativeSelect";
 import {makeStyles} from "@material-ui/core/styles";
-import SaveIcon from "@material-ui/icons/Save";
-import Modal from "@material-ui/core/Modal";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import CancelIcon from '@material-ui/icons/Cancel';
+import Dialog from "@material-ui/core/Dialog";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import { DialogContent, DialogTitle, DialogActions, option } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
+const useStyles = makeStyles({
+    paperFullWidth:{
+        overflowY: 'visible'
     },
-    button: {
-        margin: theme.spacing(1),
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: '25ch',
-    },
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
-function getModalStyle() {
-    const top = 50;
-    const left = 50;
+    DialogContentRoot:{
+        overflowY: 'visible'
+    }
+});
 
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
+
 const AddRecordModal = () => {
     const classes = useStyles();
-    const [modalStyle] = useState(getModalStyle);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
     const [open, setOpen] = useState(false);
+    const [scroll, setScroll] = useState('paper');
 
     //should be managed from parent component
     //but for now let's stick with a dummy object
@@ -100,104 +87,186 @@ const AddRecordModal = () => {
         console.log("False");
         return false;
     }
+
+
     const body = (
-        <div style={modalStyle} className={classes.paper}>
-            <h2 id="simple-modal-title">Dodawanie rekordu</h2>
-            <form className={classes.root} noValidate autoComplete="off" onSubmit={handleOnSubmit}>
-                <TextField required id="author" label="Twórca/Twórcy" style={{margin:8}} placeholder="Twórca" fullWidth
+        <>
+        <DialogTitle id="addRecordModalTitle">{"Dodaj nowy rekord"}</DialogTitle>
+        <DialogContent classes={{
+            root: classes.paperFullWidth
+        }}>
+            <Grid container spacing={1}>
+            <form noValidate autoComplete="off" onSubmit={handleOnSubmit}>
+
+                <Grid item xs={12}>
+                <TextField required id="author" label="Twórca/Twórcy"  placeholder="Twórca" fullWidth
                            margin="normal" onChange={handleOnChange}/>
-                <TextField id="coauthor" label="Współtwórca/Współtwórcy" style={{margin:8}} placeholder="Współtwórca" fullWidth
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="coauthor" label="Współtwórca/Współtwórcy"  placeholder="Współtwórca" fullWidth
                            margin="normal" onChange={handleOnChange}/>
-                <TextField required id="title" label="Tytuł" style={{margin:8}} placeholder="Tytuł" fullWidth
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField required id="title" label="Tytuł"  placeholder="Tytuł" fullWidth
                            margin="normal" onChange={handleOnChange}/>
-                <TextField id="subtitle" label="Podtytuł" style={{margin:8}} placeholder="Podtytuł" fullWidth
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="subtitle" label="Podtytuł"  placeholder="Podtytuł" fullWidth
                            margin="normal" onChange={handleOnChange}/>
-                <TextField id="origin" label="źródło" style={{margin:8}} placeholder="źródło" fullWidth
+                </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="origin" label="Źródło"  placeholder="Źródło" fullWidth
                            margin="normal" onChange={handleOnChange}/>
-                <TextField id="edition" label="Wydanie" type="number" style={{margin:8}} placeholder="Wydanie"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="place_of_issue" label="Miejsce wydania" style={{margin:8}} placeholder="Miejsce wydania"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="publisher" label="Wydawca" style={{margin:8}} placeholder="Wydawca"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="year_of_issue" label="Rok wydania" style={{margin:8}} placeholder="Rok" type="number"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="volume" label="Objętość" style={{margin:8}} placeholder="Objętość"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="series" label="Seria" style={{margin:8}} placeholder="Seria"
-                           margin="normal" onChange={handleOnChange}/>
-                <InputLabel htmlFor="language">Język</InputLabel>
-                <Select
-                    native
-                    onChange={handleOnChange}
-                    inputProps={{
-                        id: 'language',
-                    }}
-                >
-                    <option aria-label="None" value="" />
-                    <option value={"polski"}>Polski</option>
-                    <option value={"angielski"}>Angielski</option>
-                    <option value={"niemiecki"}>Niemiecki</option>
-                </Select>
-                <InputLabel htmlFor="type">Typ</InputLabel>
-                <Select
-                    native
-                    onChange={handleOnChange}
-                    inputProps={{
-                        id: 'type',
-                    }}
-                >
-                    <option aria-label="None" value="" />
-                    <option value={"type1"}>Type1</option>
-                    <option value={"type2"}>Type2</option>
-                    <option value={"type3"}>Type3</option>
-                </Select>
-                <InputLabel htmlFor="format">Format</InputLabel>
-                <Select
-                    native
-                    onChange={handleOnChange}
-                    inputProps={{
-                        id: 'format',
-                    }}
-                >
-                    <option aria-label="None" value="" />
-                    <option value={"format1"}>Format1</option>
-                    <option value={"format2"}>Format2</option>
-                    <option value={"format3"}>Format3</option>
-                </Select>
-                <TextField id="source_link" label="Link do zasobu" style={{margin:8}} placeholder="Link do zasobu" fullWidth
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="ide" label="Identyfikator" style={{margin:8}} placeholder="Identyfikator"
-                           margin="normal" onChange={handleOnChange}/>
-                <TextField id="tags" label="Treść zasobu" style={{margin:8}} placeholder="Treść zasobu"
-                           margin="normal" onChange={handleOnChange} />
-                <TextField id="comment" label="Komentarz" multiline rowsMax={4} fullWidth
+                </Grid>
+                
+                <Grid container spacing={2}>
+                    <Grid item xs>
+                    <TextField id="edition" label="Wydanie" type="number"  placeholder="Wydanie"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                    <Grid item xs>
+                    <TextField id="place_of_issue" label="Miejsce wydania"  placeholder="Miejsce wydania"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                    <Grid item xs>
+                    <TextField id="publisher" label="Wydawca"  placeholder="Wydawca"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs>
+                    <TextField id="year_of_issue" label="Rok wydania"  placeholder="Rok" type="number"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                    <Grid item xs>
+                    <TextField id="volume" label="Objętość"  placeholder="Objętość"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                    <Grid item xs>
+                    <TextField id="series" label="Seria"  placeholder="Seria"
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                </Grid>
+                   
+                    <Grid container spacing={2}>
+                        <Grid item xs>
+                        <InputLabel htmlFor="language">Język</InputLabel> 
+                         <Select
+                            native
+                            onChange={handleOnChange}
+                            inputProps={{
+                                id: 'language',
+                            }}
+                        >
+                             {/* Muszą to być optiony, poniewaz MenuItem się buguje w Dialogu */}
+                            <option aria-label="None" value={""}> </option>
+                            <option value={"polski"}>Polski</option>
+                            <option value={"angielski"}>Angielski</option>
+                            <option value={"niemiecki"}>Niemiecki</option>
+                        </Select>
+                        </Grid>
+                        <Grid item xs>
+                        <InputLabel htmlFor="type">Typ</InputLabel>
+                        <Select
+                            native
+                            onChange={handleOnChange}
+                            inputProps={{
+                                id: 'type',
+                            }}
+                        >
+                            <option aria-label="None" value={""}> </option>
+                            <option value={"type1"}>Type1</option>
+                            <option value={"type2"}>Type2</option>
+                            <option value={"type3"}>Type3</option>
+                        </Select>
+                        </Grid>
+                        <Grid item xs>
+                        <InputLabel htmlFor="format">Format</InputLabel>
+                        <Select
+                            native
+                            onChange={handleOnChange}
+                            inputProps={{
+                                id: 'format',
+                            }}
+                        >
+                            <option aria-label="None" value={""}> </option>
+                            <option value={"format1"}>Format1</option>
+                            <option value={"format2"}>Format2</option>
+                            <option value={"format3"}>Format3</option>
+                        </Select>
+                        </Grid>
+                    </Grid>
+                            
+                    <Grid item xs={12}>
+                    <TextField id="source_link" label="Link do zasobu"  placeholder="Link do zasobu" fullWidth
+                            margin="normal" onChange={handleOnChange}/>
+                    </Grid>
+                    <Grid container spacing={1}>
+                        <Grid item xs>
+                        <TextField id="ide" label="Identyfikator" placeholder="Identyfikator"
+                                margin="normal" onChange={handleOnChange}/>
+                        </Grid>
+                        <Grid item xs>
+                        <TextField id="tags" label="Treść zasobu"  placeholder="Treść zasobu"
+                                margin="normal" onChange={handleOnChange} />
+                        </Grid>
+                    </Grid>
+
+                <Grid item xs={12}>
+                <TextField id="comment" label="Komentarz" multiline rows={4} fullWidth
                            onChange={handleOnChange} />
+                </Grid>
+
+        <DialogActions>
+            <Grid container>
+            <Grid item xs>
                 <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    type="submit"
-                    className={classes.button}
-                    startIcon={<SaveIcon />}
+                onClick={handleCloseModal} 
+                variant="contained"
+                color="secondary"
+                size="large"
+                startIcon={<CancelIcon />}
+                autoFocus 
                 >
+                    Anuluj
+                </Button>
+            </Grid>
+            <Grid item xs>
+                <Button 
+                variant="contained"
+                color="primary"
+                size="large"
+                type="submit"
+                startIcon={<CloudUploadIcon />}
+                autoFocus>
                     Dodaj rekord
                 </Button>
+            </Grid>
+        </Grid>
+        </DialogActions>
             </form>
-        </div>
+        </Grid>
+        </DialogContent>
+        </>
     );
 
     return (
         <div>
             <button onClick={handleOpenModal}>Dodaj nowy rekord</button>
-            <Modal
+            <Dialog
                 open={open}
                 onClose={handleCloseModal}
+                scroll={scroll}
+                fullScreen={fullScreen}
                 aria-labelledby="simple-modal-title"
                 aria-describedby="simple-modal-description"
             >
                 {body}
-            </Modal>
+            </Dialog>
         </div>
     );
 };
